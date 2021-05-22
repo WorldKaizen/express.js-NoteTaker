158535d7377748fd4d68bc27e8e87273f08b9850
WorldKaizen <73084934+WorldKaizen@users.noreply.github.com>
2021-05-21 19:10:09 -0700

    Update api-routes.js

--- apiRoutes/api-routes.js
+++ apiRoutes/api-routes.js
@@ -1,1 +1,19 @@
 
+const express= require('express')
+const app=express();
+const path =require('path')
+const fs = require('fs')
+
+module.exports=(app)=>{
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+  
+      var notes = JSON.parse(data);
+  
+      
+  
+     
+      app.get("/api/notes", function (req, res) {
+       
+        res.json(notes);
+      });
